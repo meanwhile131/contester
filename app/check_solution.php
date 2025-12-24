dd9e8240446cdbc668e285ac72f9195e534c678a
meanwhile131 <51206659+meanwhile131@users.noreply.github.com>
2025-12-24 14:03:55 +0400

    Add queueing submitted solutions

--- app/check_solution.php
+++ app/check_solution.php
@@ -14,7 +14,6 @@ else {
     header("Location: /");
     exit();
 }
-header('X-Accel-Buffering: no');
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -29,62 +28,23 @@ header('X-Accel-Buffering: no');
 <body>
     <?php
     ob_implicit_flush(1);
-    echo "Подготовка к выполнению тестов...<br>";
     $challenge_id = $_POST["id"];
+    $code = $_POST["code"];
+    if (empty($challenge_id) || empty($code)) {
+        echo "Нет всех параметров запроса!";
+        exit();
+    }
     echo "Задача: $challenge_id<br>";
-    $tests_result = pg_query_params($db, "SELECT * FROM tests WHERE challenge=$1", [$challenge_id]);
-    $tests = pg_fetch_all($tests_result, PGSQL_ASSOC);
-
-    $descriptorspec = [["pipe", "r"], ["pipe", "w"], ["pipe", "w"]];
-    $file_path = "/tmp/" . bin2hex(random_bytes(6)) . ".py";
-    file_put_contents($file_path, $_POST["code"]);
-    $failed = false;
-    echo "Выполнение тестов...<br><br>";
-
-    foreach ($tests as $test) {
-        $input = $test["in"] . "\n";
-        $expected_output = $test["out"] . "\n";
-        $process = proc_open(
-            "timeout 3s /usr/bin/python3 $file_path",
-            $descriptorspec,
-            $pipes
-        );
-        $start = microtime(true);
-        fwrite($pipes[0], $input);
-        $output = stream_get_contents($pipes[1]);
-        $err_output = stream_get_contents($pipes[2]);
-        if ($output == $expected_output) {
-            $time = round((microtime(true) - $start) * 1000, 2);
-            echo "Тест пройден за {$time} мс!<br>";
-        } else {
-            if (proc_get_status($process)["exitcode"] != 124) {
-                echo "Тест не пройден! <br> <pre>$err_output $output</pre> <br>";
-            } else {
-                echo "Ограничение по времени!<br>";
-            }
-            $failed = true;
-            break;
-        }
-        fclose($pipes[0]);
-        fclose($pipes[1]);
-        fclose($pipes[2]);
-        proc_close($process);
+    pg_query($db, "BEGIN;");
+    $result = pg_query_params($db, "INSERT INTO solutions(challenge,code) VALUES ($1,$2);", [$challenge_id, $code]);
+    if (pg_affected_rows($result) == 0) {
+        pg_query($db, "ROLLBACK");
+        echo "Ошибка при добавлении задачи в очередь!";
+        exit();
     }
-    unlink($file_path);
-    echo "<br>";
-    if ($failed) {
-        echo "Тесты не пройдены!";
-    } else {
-        echo "Тесты пройдены! Сохраняем результат...<br>";
-        $query = pg_query_params($db, "UPDATE users SET tasks_solved= tasks_solved | (1<<$1) WHERE sub=$2", [intval($challenge_id) - 1, $userid]);
-        if (pg_affected_rows($query) == 1) {
-            echo "Результат сохранен!";
-        }
-        else {
-            echo "Ошибка при сохранении результата!";
-        }
-    }
-    echo "</p>";
+    pg_query($db, "NOTIFY solution_submitted;");
+    pg_query($db, "COMMIT;");
+    echo "Задача добавлена в очередь";
     ?>
 </body>
 
--- runner/src/main.cpp
+++ runner/src/main.cpp
@@ -1,5 +1,72 @@
 #include <iostream>
+#include <pqxx/pqxx>
+#include <queue>
 
-int main() {
-    std::cout << "Hello world!" << std::endl;
+void queue_submitted_solutions(pqxx::work &tx, std::queue<uint64_t> &queue)
+{
+    auto submitted_solutions = tx.query<uint64_t>("UPDATE solutions SET status='queued' WHERE status='submitted' RETURNING id;");
+    for (auto [id] : submitted_solutions)
+    {
+        queue.push(id);
+        std::cout << "Queued #" << id << "\n";
+    }
+}
+
+int main()
+{
+    std::queue<uint64_t> queued_solutions;
+    std::string db_connection_string = std::getenv("RUNNER_PGSQL_CONNECTION");
+    pqxx::connection c{db_connection_string};
+    std::cout << "Connected to " << c.dbname() << '\n';
+
+    c.listen("solution_submitted", [&c, &queued_solutions](pqxx::notification notification)
+             {
+        pqxx::work tx{c};
+        queue_submitted_solutions(tx, queued_solutions);
+        tx.commit(); });
+    pqxx::work tx{c};
+    queue_submitted_solutions(tx, queued_solutions);                                                     // queue solutions submitted before listen
+    auto solutions_marked_queued = tx.query<uint64_t>("SELECT id FROM solutions WHERE status='queued';"); // requeue solutions already marked queued
+    for (auto [id] : solutions_marked_queued)
+    {
+        queued_solutions.push(id);
+        std::cout << "Add solution #" << id << " to queue\n";
+    }
+    tx.commit();
+    std::cout << "Starting main loop\n";
+    while (true)
+    {
+        if (queued_solutions.empty())
+            c.await_notification();
+        if (queued_solutions.empty())
+            continue; // no solutions were queued
+        uint64_t id = queued_solutions.front();
+        std::cout << "Processing #" << id << "\n";
+        pqxx::result result;
+        queued_solutions.pop();
+        {
+            pqxx::work tx{c};
+            result = tx.exec("SELECT code FROM solutions WHERE id=$1", id);
+            if (result.size() != 1)
+            {
+                std::cerr << "Cannot find solution #" << id << " from local queue in DB\n";
+                continue;
+            }
+            tx.exec("UPDATE solutions SET status='testing' WHERE id=$1", id);
+            tx.commit();
+        }
+        std::cout << "Testing #" << id << "\n";
+
+
+        std::string code = result[0][0].c_str();
+        std::cout << code << "\n";
+
+        {
+            pqxx::work tx{c};
+            tx.exec("UPDATE solutions SET status='done' WHERE id=$1", id);
+            tx.commit();
+        }
+        std::cout << "#" << id << " done\n";
+    }
+    return 0;
 }
